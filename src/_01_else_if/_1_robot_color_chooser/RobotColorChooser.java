117a4bf13f3ad9c41328d9d1fbc415b655aef0e2
league <league@192.168.1.214>
2024-03-03 11:27:00 -0800

    I had did one by myself

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -13,17 +13,63 @@ public class RobotColorChooser {
     	Robot rob = new Robot();
     	rob.penDown();
     	rob.setPenWidth(10);
+    	rob.setSpeed(200);
     	
-    	String input=JOptionPane.showInputDialog("what color do you want in this list. Red,Blue,Green,hot pink and navy purple");
+       int loop=0;
+       for (loop=0; loop < 9999; loop+=1) {
+		int count =0;
+		
+    	String input = JOptionPane.showInputDialog("what color do you want in this list.Red,Blue,Green,hot pink and navy purple");
     	 if(input.equals("red")) {
-    	rob.setPenColor(100, 0, 0);
-    	rob.setSpeed(1000);
-    	int count = 0;
-         while ( count < 4 ){
-     	rob.move(300);
-     	rob.turn(90);       
-     	count += 1;
+    		rob.setPenColor(170,10,10);
+    		 while ( count <4 ){
+    		    	rob.move(100);
+    			 rob.turn(90);
+    			 count += 1;
+    		 
+    			
+    		 }
     	}
+    	 else if(input.equals("blue")) {
+    		 rob.setPenColor(0,0,160);
+    		 while ( count < 5 ){
+    			 rob.move(89);
+    			 rob.turn(72);
+    			 count += 1;
+    		 }
+    	 }  
+    	 else if (input.equals("green")){ 
+    		 rob.setPenColor(10,160,0);
+    		 while ( count <10 ){
+    			 rob.move(70);
+    			 rob.turn(36);
+    			 count += 1;
+    		 }
+    	 }
+    	 else if (input.equals("hot pink")){
+     		 rob.setPenColor(160,30,15);
+     		 while ( count < 10 ){
+     			 rob.move(70);
+     			 rob.turn(36);
+     			 count += 1;
+     		 }
+     	 }
+    	 else if(input.equals("navy purple")) {
+     		 rob.setPenColor(160,1,160);
+     		 while ( count < 12 ){
+     			 rob.move(70);
+     			 rob.turn(30);
+     			 count += 1;
+     		 }
+     	 } else  {
+     		rob.setRandomPenColor();
+    		 while ( count < 12 ){
+    			 rob.move(70);
+    			 rob.turn(30);
+    			 count += 1;
+     	 }
+     	 }
+    		}
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,6 +3,8 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
@@ -13,13 +15,45 @@ public class CrazyCatLady {
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
-		
-		
-		
+		int loop=0;
+		for (loop=0; loop <= 9999; loop+=1) {
+		String input = JOptionPane.showInputDialog("Say how many cats you had if you had killed cats minus one");
+		int num = Integer.parseInt(input);
+		if(num==3) {
+			JOptionPane.showMessageDialog(null, ("crazy cat lady"));
+			
+		}
+		if(num==2) {
+			JOptionPane.showMessageDialog(null, ("Are they husband and wife?"));
+			
+		}
+		else if(num==1) {
+			JOptionPane.showMessageDialog(null, ("You are trying it out? nice"));
+			
+		}
+		else if(num==0) {
+			JOptionPane.showMessageDialog(null, ("Youre a dog person"));
+			
+		}
+		else if(num==-1) {
+			JOptionPane.showMessageDialog(null, ("took that cat biteing you to another level"));
+			
+		}
+		else if(num==-2) {
+			JOptionPane.showMessageDialog(null, ("where you in a gang fight"));
+			
+		}
+		else if(num==-3) {
+			JOptionPane.showMessageDialog(null, ("The Puston massacre"));
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(null, ("No comment to much or to little or you said three instead of 3"));
+		}
 		
 		
 	}
-
+	}
 	static void playVideo(String videoURL) {
 		try {
 			URI uri = new URI(videoURL);
